50d8e382f3672eea7005bf682d22398489ea7c87
arivusurya <arivusurya.3@gmail.com>
2023-02-27 23:30:14 +0530

    search fuction added

--- Program.cs
+++ Program.cs
@@ -18,12 +18,15 @@ public class Program{
             bst.Insert(60);
             bst.Insert(95);
             bst.Insert(65);
-
-            Console.WriteLine("Inorder Traversal of BST:");
-            bst.InorderTraversal(bst.root);
-
-            Console.WriteLine("\n");
-            Console.WriteLine("Size of BST: " + bst.Size(bst.root));
+             Node node63 = bst.Search(63);
+                if (node63 == null)
+                 {
+                    Console.WriteLine("Node not found");
+                 }
+                else
+                {
+                    Console.WriteLine("Node found: " + node63.data);
+                }
            
     }
 
