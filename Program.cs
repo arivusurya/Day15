3fb2931738020556fa75f17768f5b29f8e12f462
arivusurya <arivusurya.3@gmail.com>
2023-02-27 23:19:24 +0530

    insert an element

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Day15BinarySearchTree;
+
+public class Program{
+    public static void Main(string[] args)
+    {
+        Day15BinarySearchTree.BinarySearchTree bst = new BinarySearchTree();
+            bst.Insert(56);
+            bst.Insert(30);
+            bst.Insert(70);
+    }
+
+}
